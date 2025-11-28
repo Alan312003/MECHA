beb9a8124ec1bf6cdb37713d51bbcdf5efa3e1ec
pedro <pedro_rf1@tesch.edu.mx>
2025-11-28 12:41:53 -0600

    Modificaciones al Login

--- app/src/main/java/com/example/mecha/LoginActivity.kt
+++ app/src/main/java/com/example/mecha/LoginActivity.kt
@@ -1,6 +1,10 @@
 package com.example.mecha
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,27 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val edtcorreo = findViewById<EditText>(R.id.edtCorreo)
+        val edtpassword = findViewById<EditText>(R.id.edtPassword)
+        val btningresar= findViewById<Button>(R.id.btnLogin)
+        val btnregistrar = findViewById<Button>(R.id.btnRegistrar)
+        val tvdatos = findViewById<TextView>(R.id.tvDatos)
+
+        btningresar.setOnClickListener {
+            val correo = edtcorreo.text.toString()
+            val psw= edtpassword.text.toString()
+
+            if (correo.isNotEmpty() && psw.isNotEmpty()){
+                Toast.makeText(this, "Campos llenos", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                tvdatos.text = "Llena todos los campos."
+                Toast.makeText(this, "faltan llenar los campos", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+
     }
 }
